035ae4322212834d1c938fb669e173f235a54e0f
Jude Ketel <jude.ketel@protonmail.com>
2021-10-15 14:11:13 -0400

    Completed rock paper scissors assignment

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 
 let selection = ["Rock" , "Paper", "Scissors"]
 
+let playerScore = 0, computerScore = 0;
+
 // computer chooses Rock, Paper or Scissors
 
 function computerPlay() {
     let randomPick = selection[Math.floor(Math.random() * selection.length)];
-    alert(randomPick);
+    return randomPick;
 }
 
 // single round of play
@@ -16,36 +18,72 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
     playerSelection = prompt('Please choose Rock, Paper, or Scissors', );
 
+    // Below two lines allows input to be case insensitive
+
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
+    console.log('Player chose ' + playerSelection);
+    console.log('Computer chose ' + computerSelection);
+
     if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-        alert('Rock beats Scissors. Player wins!');
+        console.log('Rock beats Scissors. Player wins!');
+        playerScore = playerScore + 1;
     }
     else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-        alert('Paper beats Rock. Computer wins!');
+        console.log('Paper beats Rock. Computer wins!');
+        computerScore = computerScore + 1;
     }
-    else if (playSelection == 'Paper' && computerSelection == 'Rock') {
-        alert('Paper beats Rock. Player wins!');
+    else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        console.log('Paper beats Rock. Player wins!');
+        playerScore = playerScore + 1;
     }
     else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
-        alert('Scissors beats Paper. Computer wins!');
+        console.log('Scissors beats Paper. Computer wins!');
+        computerScore = computerScore + 1;
     }
     else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
-        alert('Rock beats Scissors. Computer wins!');
+        console.log('Rock beats Scissors. Computer wins!');
+        computerScore = computerScore + 1;
     }
     else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-        alert('Scissors beats Paper. Player wins!');
+        console.log('Scissors beats Paper. Player wins!');
+        playerScore = playerScore + 1;
     }
     else if (playerSelection == 'Paper' && computerSelection == 'Paper') {
-        alert("Both parties chose Paper. It's a draw");
+        console.log("Both parties chose Paper. It's a draw");
     }
     else if (playerSelection == 'Rock' && computerSelection == 'Rock') {
-        alert("Both parties chose Rock. It's a draw");
+        console.log("Both parties chose Rock. It's a draw");
     }
     else if (playerSelection == 'Scissors' && computerSelection == 'Scissors') {
-        alert("Both parties chose Scissors. It's a draw");
+        console.log("Both parties chose Scissors. It's a draw");
     }
     else {
-        alert('Something went wrong');
+        console.log('Something went wrong. Please retry and enter a valid option');
     }
 }
 
-playRound();
+function game() {
+
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+
+    console.log('Player Score: ' + playerScore + ' Computer Score: ' + computerScore); // Final scores displayed
+
+    if (playerScore > computerScore) {
+        console.log('Player wins the game!')
+    }   
+    else if (computerScore > playerScore) {
+        console.log('Computer wins the game!')
+    }
+    else {
+        console.log("It's a draw!")
+    }
+    
+}
+
+game();
